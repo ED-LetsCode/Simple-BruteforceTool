e475d725c2d9c1304cebbcc1e399685a49a7de90
ED-LetsCode <eray.demirel@lets-code.at>
2022-12-12 20:11:37 +0100

    Projektdateien hinzufügen.

---
+++ BruteforceConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using BruteforceLibrary;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace BruteforceConsole
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"§$%&/()=?*+'#äÄöÖ-_.:,;<>";
+            Bruteforce bruteforce = new Bruteforce();
+            int passwordLength = 5;
+            
+            long possiblePasswords = bruteforce.CalculatePossiblePasswords(passwordLength, characters);
+            Console.WriteLine($"Possible passwords: {possiblePasswords}");
+            IEnumerable<string> passwords = null;
+            
+            for (long i = 0; i <= possiblePasswords; i++)
+            {
+                passwords = bruteforce.GeneratePassword(characters, passwordLength);
+            }
+
+            passwords.ToList().ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
---
+++ BruteforceLibrary/Bruteforce.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BruteforceLibrary
+{
+    public class Bruteforce
+    {
+
+        public IEnumerable<string> GeneratePassword(string characters, int length)
+        {
+            if (length == 0) yield return "";
+            else
+            {
+                foreach (char values in characters)
+                {
+                    foreach (string value in GeneratePassword(characters, length - 1))
+                    {
+                        yield return values + value;
+                    }
+                }
+            }
+        }
+
+        public long CalculatePossiblePasswords(int passwordLength, string passwordCharacters) => (long)Math.Pow(passwordCharacters.Length, passwordLength);
+    }
+}
